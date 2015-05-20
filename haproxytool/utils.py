46e22478b832bb21cde9e4b2916a890786deef47
Pavlos Parissis <pavlos.parissis@gmail.com>
2015-05-20 21:06:46 +0200

    utils.py: switch to gmail account

--- haproxytool/utils.py
+++ haproxytool/utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
 #
-# Created by: Pavlos Parissis <pavlos.parissis@booking.com>
+# Created by: Pavlos Parissis <pavlos.parissis@gmail.com>
 #
 import re
 from operator import attrgetter
