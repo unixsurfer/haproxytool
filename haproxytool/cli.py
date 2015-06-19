33ccc2b219104ffc9c0c593ac7d4547715f60add
Pavlos Parissis <pavlos.parissis@gmail.com>
2015-06-19 20:23:48 +0200

    bump release

--- haproxytool/cli.py
+++ haproxytool/cli.py
@@ -30,7 +30,7 @@ from operator import methodcaller
 
 
 def main():
-    args = docopt(__doc__, version='haproxytool 0.0.10', options_first=True)
+    args = docopt(__doc__, version='haproxytool 0.1.0', options_first=True)
 
     call_main = methodcaller('main')
 
