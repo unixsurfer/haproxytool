ffddbaba2fa3b7934608c28d5c714a7c2c9cfa11
Pavlos Parissis <pavlos.parissis@gmail.com>
2015-06-26 12:44:36 +0200

    RELEASE 0.3.0

--- haproxytool/cli.py
+++ haproxytool/cli.py
@@ -29,7 +29,7 @@ from operator import methodcaller
 
 
 def main():
-    args = docopt(__doc__, version='haproxytool 0.2.2', options_first=True)
+    args = docopt(__doc__, version='haproxytool 0.3.0', options_first=True)
 
     call_main = methodcaller('main')
 
