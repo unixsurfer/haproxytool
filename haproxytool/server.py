0ff34fc09706ffbbd8b52e5b86f8afc1296fb6cc
Pavlos Parissis <pavlos.parissis@gmail.com>
2015-10-02 21:40:14 +0200

    order server options alphabetically

--- haproxytool/server.py
+++ haproxytool/server.py
@@ -22,22 +22,22 @@ Arguments:
     METRIC  Name of a metric, use '-M' to get metric names
 
 Options:
-    -h, --help                show this screen
-    -e, --enable              enable server
     -d, --disable             disable server
-    -R, --ready               set server in normal mode
-    -n, --drain               drain server
-    -t, --maintenance         set server in maintenance mode
-    -r, --requests            show requests
-    -p, --process             show process number
+    -e, --enable              enable server
+    -f, --force               force an operation
+    -h, --help                show this screen
     -i, --sid                 show server ID
-    -s, --status              show status
+    -l, --list                show all servers
     -m, --metric              show value of a metric
     -M, --list-metrics        show all metrics
-    -l, --list                show all servers
+    -n, --drain               drain server
+    -p, --process             show process number
+    -r, --requests            show requests
+    -R, --ready               set server in normal mode
+    -s, --status              show status
+    -t, --maintenance         set server in maintenance mode
     -w, --weight              change weight for server
     -W, --get-weight          show weight of server
-    -f, --force               force an operation
     -D DIR, --socket-dir=DIR  directory with HAProxy socket files
                               [default: /var/lib/haproxy]
 
