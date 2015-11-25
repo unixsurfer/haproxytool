ed208a96d50aa8ae33a30884582cd4c71f041f9b
Pavlos Parissis <pavlos.parissis@booking.com>
2015-11-25 14:17:06 +0100

    RELEASE 0.4.4 version

--- haproxytool/__init__.py
+++ haproxytool/__init__.py
@@ -9,6 +9,6 @@
 #             |_|                  |___/'
 __title__ = 'haproxytool'
 __author__ = 'Pavlos Parissis'
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2015 Pavlos Parissis'
