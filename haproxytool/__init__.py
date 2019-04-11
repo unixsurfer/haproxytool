e8f008e6e4168b818691d4b4c389afbbf584771b
Pavlos Parissis <pavlos.parissis@booking.com>
2019-04-12 00:21:58 +0200

    Make __init__.py more pylint friendly
    We also update Copyright.

--- haproxytool/__init__.py
+++ haproxytool/__init__.py
@@ -1,23 +1,10 @@
-# -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-#
-# _                                     _              _
-# | |__   __ _ _ __  _ __ _____  ___   _| |_ ___   ___ | |
-# | '_ \ / _` | '_ \| '__/ _ \ \/ / | | | __/ _ \ / _ \| |
-# | | | | (_| | |_) | | | (_) >  <| |_| | || (_) | (_) | |
-# |_| |_|\__,_| .__/|_|  \___/_/\_\\__, |\__\___/ \___/|_|
-#             |_|                  |___/'
-"""
-haproxytool
-~~~~~~~~~~~
-
-A tool to manage HAProxy via stats socket.
-"""
+"""haproxytool a tool to manage HAProxy via stats socket."""
 __title__ = 'haproxytool'
 __author__ = 'Pavlos Parissis'
 __version__ = '0.5.4'
 __license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2015-2016 Pavlos Parissis'
+__copyright__ = 'Copyright 2015-2019 Pavlos Parissis'
 
 OUR_CMDS = [
     'haproxy',
